3c83f97e8bb2fb93a907d53c054f9aed5247ee7f
Manoj Baskar <147849968+manojaba@users.noreply.github.com>
2024-11-14 10:03:35 +0530

    MyApp.js created

---
+++ src/MyApp.js
@@ -0,0 +1,18 @@
+function Button() {
+    return (
+        <>
+            <button>this is a button</button>
+        </>
+    )
+}
+
+function MyApp() {
+    return (
+        <>
+            <h1>this is my app  component</h1>
+            <Button />
+        </>
+    )
+}
+
+export default MyApp
